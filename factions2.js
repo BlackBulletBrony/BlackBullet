c6232a54745a22b379437f7f37d0cfb69af5b375
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-07-15 11:08:23 +0500

    Create factions2.js

---
+++ factions2.js
@@ -0,0 +1,10 @@
+{
+    "MLP Pixel":{"url":"https://raw.githubusercontent.com/Autumn-Blaze/ponehs/master/","color":"#1992E3","type":"1","templates":"mine"},
+    "New Lunar Republic":{"url":"https://endlessnightnlr.github.io/master/","color":"aqua","type":"1","templates":"own"},
+    "...":{"url":"https://raw.githubusercontent.com/rony2627/1234567890/master/","color":"grey","type":"1","templates":"own"},
+    "Lyra":{"url":"https://raw.githubusercontent.com/lyrahearts/bbb/master/","color":"#35FF35","type":"1","templates":"own"},
+    "hz":{"url":"https://raw.githubusercontent.com/ShoNCUdoS/fix/master/","color":"#35FF35","type":"1","templates":"own"},
+    "BLaCKBuLLeT":{"url":"https://raw.githubusercontent.com/BlackBulletBrony/BlackBulletBrony/master/","color":"#262626","type":"1","templates":"own"},
+    "Argentina":{"url":"https://raw.githubusercontent.com/Marxarg/pz-argentina/master/","color":"aqua","type":"1","templates":"own"},
+    "Gilda":{"url":"https://raw.githubusercontent.com/Marcospz/pz-templates/master/","color":"aqua","type":"1","perm":"1","templates":"own"}
+}
